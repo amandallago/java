be51b38a8e769f4ea4ce69d6f8aa2051e48847fd
amandallago <amandslago@gmail.com>
2025-08-20 19:34:41 -0300

    New commit

---
+++ java/vetores/Ex0.java
@@ -0,0 +1,26 @@
+package java;
+
+import java.util.Scanner;
+
+public class Ex0 {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	int[] vetor1 = new int[4];
+	int[] vetor2 = new int[4];
+	int [] vetorSoma = new int[4];
+	
+	System.out.println("");		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
